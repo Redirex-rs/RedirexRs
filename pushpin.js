a60f862f188e9fd6f5573e6c3fefb5f814e8ec09
Redirex-rs <stankovicv93@gmail.com>
2019-11-02 18:03:45 +0100

    first commit

---
+++ pushpin.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.pushpin');
+    var instances = M.Pushpin.init(elems, options);
+  });// JavaScript Document
