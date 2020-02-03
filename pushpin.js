21b073bb91809daf30d26bbb6836e5e55ef54efe
Redirex-rs <stankovicv93@gmail.com>
2020-02-03 19:46:55 +0100

    sufficient

--- pushpin.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.pushpin');
-    var instances = M.Pushpin.init(elems, options);
-  });// JavaScript Document
