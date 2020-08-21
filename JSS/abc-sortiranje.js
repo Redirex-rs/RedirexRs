154f29eb0ea75dd4c447722cf4bafa74111188d0
Redirex-rs <57158117+Redirex-rs@users.noreply.github.com>
2020-08-21 05:16:40 +0200

    14

---
+++ JSS/abc-sortiranje.js
@@ -0,0 +1,14 @@
+var activeLanguage = "de"
+
+function sortUL(selector) {
+var $ul = $(selector);
+$ul.find('li').sort(function (a, b) {
+    var upA = $(a).text().toUpperCase();
+    var upB = $(b).text().toUpperCase();
+    return (upA < upB) ? -1 : (upA > upB) ? 1 : 0;
+}).appendTo(selector);
+};
+
+$(document).ready(function () {
+sortUL("#lista-gradova");
+});
