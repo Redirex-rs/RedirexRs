0b561568e3af79147f4b2295656c6a12bb5ff8a1
Redirex-rs <57158117+Redirex-rs@users.noreply.github.com>
2020-08-24 15:22:18 +0200

    15.5

--- JSS/W3-search.js
+++ JSS/W3-search.js
@@ -1,19 +1,19 @@
 function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    ul.style.display = "block";
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    if (filter.split('')
+      .every(x => txtValue.toUpperCase().split('').some(y => y === x))) {
       li[i].style.display = "";
     } else {
       li[i].style.display = "none";
     }
   }
 }
+
