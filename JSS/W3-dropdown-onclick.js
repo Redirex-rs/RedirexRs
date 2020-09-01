3403ccd8887b143426bdd8f0f60a2559b9b5d86f
Redirex-rs <57158117+Redirex-rs@users.noreply.github.com>
2020-09-01 20:46:04 +0200

    17-big

--- JSS/W3-dropdown-onclick.js
+++ JSS/W3-dropdown-onclick.js
@@ -4,6 +4,9 @@ $(function(){
             $(this).find(".dropdown-menu").slideToggle("fast");
         });
     });
+$('.dropdown').click(function() {
+    $(this).toggleClass('clicked');
+});
     $(document).on("click", function(event){
         var $trigger = $(".dropdown");
         if (!$(event.target).closest('.dropdown').length){
