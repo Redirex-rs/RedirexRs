b2aeb306e737072757d75078ea399fcacf292895
Redirex-rs <57158117+Redirex-rs@users.noreply.github.com>
2020-09-21 21:34:23 +0200

    18
    dugmici, etc

---
+++ css/Overlay.js
@@ -0,0 +1,43 @@
+	
+	$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+		  
+		  /* Open when someone clicks on the span element */
+/* Open when someone clicks on the span element */
+function openNav1() {
+  document.getElementById("myNav1").style.height = "75%";
+}
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav1() {  document.getElementById("myNav1").style.height = "0%";
+}; 
+	
+
+		  /* Open when someone clicks on the span element */
+/* Open when someone clicks on the span element */
+function openNav2() {
+  document.getElementById("myNav2").style.height = "75%";
+}
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav2() {  document.getElementById("myNav2").style.height = "0%";
+}; 
+	
+		  /* Open when someone clicks on the span element */
+/* Open when someone clicks on the span element */
+function openNav3() {
+  document.getElementById("myNav3").style.height = "75%";
+}
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav3() {  document.getElementById("myNav3").style.height = "0%";
+}; 	
+
+	
+		  /* Open when someone clicks on the span element */
+/* Open when someone clicks on the span element */
+function openNav4() {
+  document.getElementById("myNav4").style.height = "75%";
+}
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav4() {  document.getElementById("myNav4").style.height = "0%";
+}; 	
+// JavaScript Document
