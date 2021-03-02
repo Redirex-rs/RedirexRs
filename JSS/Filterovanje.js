35300d21f0609b2a6d7434c4bd318154d969dadd
Redirex-rs <57158117+Redirex-rs@users.noreply.github.com>
2021-03-02 19:37:45 +0100

    asd

--- JSS/Filterovanje.js
+++ JSS/Filterovanje.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#myInput").on("keyup", function() {
+  $("#myInput1").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $("#myList li").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
