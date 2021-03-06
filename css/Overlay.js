21abd570700e2601d92188e83f71f319dfc5dfbc
Redirex-rs <57158117+Redirex-rs@users.noreply.github.com>
2021-03-06 01:53:03 +0100

    asd

--- css/Overlay.js
+++ css/Overlay.js
@@ -2,42 +2,15 @@
 	$(document).ready(function(){
     $('.sidenav').sidenav();
   });
-		  
-		  /* Open when someone clicks on the span element */
-/* Open when someone clicks on the span element */
-function openNav1() {
-  document.getElementById("myNav1").style.height = "75%";
-}
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav1() {  document.getElementById("myNav1").style.height = "0%";
-}; 
-	
 
-		  /* Open when someone clicks on the span element */
-/* Open when someone clicks on the span element */
-function openNav2() {
-  document.getElementById("myNav2").style.height = "75%";
-}
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav2() {  document.getElementById("myNav2").style.height = "0%";
-}; 
-	
-		  /* Open when someone clicks on the span element */
-/* Open when someone clicks on the span element */
-function openNav3() {
-  document.getElementById("myNav3").style.height = "75%";
-}
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav3() {  document.getElementById("myNav3").style.height = "0%";
-}; 	
 
-	
-		  /* Open when someone clicks on the span element */
 /* Open when someone clicks on the span element */
-function openNav4() {
-  document.getElementById("myNav4").style.height = "75%";
+function openNav() {
+  document.getElementById("myNav").style.width = "60%";
 }
+
 /* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav4() {  document.getElementById("myNav4").style.height = "0%";
-}; 	
-// JavaScript Document
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+} 
+
