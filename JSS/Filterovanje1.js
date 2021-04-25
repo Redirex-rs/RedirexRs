b242d3cb6cff5219567e7082097dd5989f8e471c
Redirex-rs <57158117+Redirex-rs@users.noreply.github.com>
2021-04-25 09:21:30 +0200

    update 3.0

--- JSS/Filterovanje1.js
+++ JSS/Filterovanje1.js
@@ -1,21 +1,8 @@
 $(document).ready(function(){
-
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
-        }
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#direktpret li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
-
+  });
 });
