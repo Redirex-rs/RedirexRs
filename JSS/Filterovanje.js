45d953ed63b11b2e4f2c5e5e9f55debc6a87acf4
Redirex-rs <57158117+Redirex-rs@users.noreply.github.com>
2022-09-25 17:13:55 +0200

    update 2.2
    glavna stranica

--- JSS/Filterovanje.js
+++ JSS/Filterovanje.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("#myInput1").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $("#myList .blog-container").filter(function() {
+    $("#myList li").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
